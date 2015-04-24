d98d3c438fe47db8d925679d56e96d31fe7174eb
miriam230 <floresmiriam685@gmail.com>
2015-04-23 22:22:35 -0700

    updated Grid.h file and created the implementation file for the Grid.h header file

---
+++ Grid.cpp
@@ -0,0 +1,59 @@
+//****************************************************
+//File Name:Grid.cpp
+//
+//Summary: The following file implements the methods for thr grid class
+//Modification:
+//Author: Miriam Flores
+//compiler: CB 13.12
+//****************************************************
+//
+#include <cassert>
+
+using namespace std;
+
+ Gird::Grid()
+ {
+    question = "";
+    answers = "";
+    grid_value = "";
+ }
+bool Grid::isQuestionChosen()
+{
+    assert(grid_value == "X");
+    return true
+
+}
+void Grid::setQuestion(string question)
+{
+    assert(question != "");
+    this -> question = question;
+
+}
+void Grid::setAnswer(string answer)
+{
+    assert(answers != "");
+    this -> question = question;
+
+}
+void Grid::setGridValue(string grid_value)
+{
+    assert(grid_Value != "");
+    this -> grid_value = grid_value;
+}
+void Grid::displayQuestion()
+{
+    cout << Question << "?\n";
+
+}
+void Grid::displayAnswer()
+{
+    cout << answers << endl;
+
+}
+bool Grid::checkAnswer()
+{
+    assert(getContestantAnswer() != "") // not sure if the getContestant method will work
+    if(getContestantAnswer() == answers) // I need to see in which class the getContestantAnswer() is
+        return true
+    return false
+};
